f6fecba7fb7fc5f28fd98dc5532dd98b4d10d441
parkerl <parker@earthvectors.com>
2010-11-02 15:13:52 -0600

    Add Rails example proj.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expenses::Application.config.secret_token = '850e6950bf0aee97d6bf317fdb26862fa015d60ff5c5956fbf78acf50792741ea1b526cc4b99010fc1a817a6a8848ccfdebcd989be3b67169e11903c7c65ec18'
